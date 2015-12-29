e2592c54bf110e35957a6c093a79999c3a1047bb
rileyjshaw <rileyjshaw@gmail.com>
2015-12-29 13:17:34 -0500

    Rewrite extension and update version to 3.0.0
    Initially, this extension grabbed the content of a CSS file with an XMLHttpRequest and injected it into the bottom of the page. This had a few advantages:
    
    1. Text content could be easy processed and manipulated (admittedly, I wasn't using this for anything).
    2. Toggling styles was as easy as adding and removing a <style> element from <body>.
    3. In theory, this strategy would beat out almost every other style rule (some inline styles excepted). [User `!important` rules used to override author `!important` rules](http://www.w3.org/TR/CSS2/cascade.html#cascading-order), but Chrome [no-longer does user stylesheets](https://src.chromium.org/viewvc/chrome?revision=234007&view=revision). I figured an aggressively `!important` author stylesheet added at the very bottom of the page was pretty solid.
    
    After some testing, I realized that `!important` styles from `content_scripts` injection (along with chrome.tabs.insertCSS) actually _do_ take precedence over author stylesheets. Since 3) was the key consideration for my original decision, I re-wrote the extension to inject a stylesheet from `content_scripts`.
    
    This change in architecture had pros and cons.
    
    ```
    + Improved chance of dark theme winning out over author styles.
    + Allowed styles to be applied before any other DOM is constructed, substantially reducing time-to-darkness.
    + Simplified the callbacks between background.js and client.js, reduced code, and made the entire extension easier to reason about.
    
    - With 1) above, I could've handled variant rules (eg. specificityHelper) with a few regular expressions. Locking into a static stylesheet added some huge copypastas, tripling the size of main.css.
    - Injected stylesheets aren't accessible once they've been added. Rather than "turning the styles off" like in 2), the best option was to add a toggle class to <body>.
    - Rewrites take time.
    ```
    
    This commit was essentially a full rewrite, so I changed some smaller things while I was at it:
    
    - Styles now look for :not(.off) instead of .on. This makes the default dark and avoids a Blinding White Flash before the class changes.
    - Added id specificity helpers; it's discussed further in client.js:24.
    - Renamed some files for clarity.
    
    I came across some unfortunate Chromium bugs while working on this, which caused me to dive into that project. It's huge! Lots of fun to poke around :)

--- background.js
+++ background.js
@@ -5,59 +5,30 @@
 		global.localStorage.setItem('theme', 'dark');
 	}
 
-	// We're going to load the text content of our CSS file so that we can inject
-	// it into a <script> tag at the bottom of <body>. This will (hopefully) win
-	// us the specificity wars against aggressively !important-ing pages.
-	var xhr = new XMLHttpRequest();
-	var cssContent = '';
+	// Chrome extensions don't currently let you listen to the extension button
+	// from content scripts, so our background script acts as a dispatcher to
+	// the active tab.
+	chrome.browserAction.onClicked.addListener(function (tab) {
+		chrome.tabs.sendMessage(tab.id, 'toggle', setIcon);
+	});
 
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState !== XMLHttpRequest.DONE) return;
+	// The active tab will, in turn, let the background script know when it has
+	// loaded new content so that we can re-initialize the tab.
+	chrome.runtime.onMessage.addListener(
+		function (request, sender, sendResponse) {
+			// Early exit if the message isn't coming from a content script.
+			var tab = sender.tab;
+			if (request !== 'ready' || !tab) {return;}
 
-		// We cache the text content of our CSS file here, so that we don't have to
-		// re-fetch it for each page load.
-		cssContent = xhr.responseText;
-
-		// Chrome extensions don't currently let you listen to the extension button
-		// from content scripts, so our background script acts as a dispatcher to
-		// the active tab.
-		chrome.browserAction.onClicked.addListener(toggleActiveTab);
-
-		// The active tab will, in turn, let the background script know when it has
-		// loaded new content so that we can re-send cssContent.
-		chrome.runtime.onMessage.addListener(
-			function (request, sender, sendResponse) {
-				if (request && sender.tab) {
-					var defaultTheme = global.localStorage.getItem('theme');
-					setIcon(defaultTheme === 'dark');
-					sendResponse({
-						cssContent: cssContent,
-						defaultTheme: defaultTheme,
-					});
-				}
-			}
-		);
-
-		if (global.localStorage.getItem('theme') === 'dark') toggleActiveTab();
-	}
-	xhr.open('GET', chrome.extension.getURL('dark-mode.css'), true);
-	xhr.send();
+			var isDark = global.localStorage.getItem('theme') === 'dark';
+			if (!isDark) {sendResponse();}
+			setIcon(isDark);
+		}
+	);
 
 	// Helper function to set the browser action icon.
 	function setIcon (isDark) {
 		var file = 'icon38' + (isDark ? '' : '-light') + '.png';
 		chrome.browserAction.setIcon({path: chrome.extension.getURL(file)});
-	}
-
-	// Finds the active tab and sends a message indicating to toggle from light
-	// to dark mode, or vice-versa.
-	function toggleActiveTab () {
-		chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-			chrome.tabs.sendMessage(
-				tabs[0].id, {cssContent: cssContent}, function (response) {
-					setIcon(response.isDark);
-				}
-			);
-		});
 	}
 }(this);
---
+++ client.js
@@ -0,0 +1,48 @@
+!function (global) {
+	var html = document.documentElement;
+	// Keeps track of the current toggle state.
+	var isDark = true;
+
+	// This should never be called by a content script outside of a listener. We
+	// expose it to background.js through listeners, and trust background.js as
+	// the dispatcher.
+	function toggle () {
+		html.classList[(isDark ? 'add' : 'remove')]('dark-theme-everywhere-off');
+		return (isDark = !isDark);
+	}
+
+	// If the background script (i.e. dispatcher) says "toggle", toggle!
+	chrome.runtime.onMessage.addListener(function (request, sender, response) {
+		// Early exit if the message is coming from another content script.
+		if (!request || sender.tab || typeof response !== 'function') {return;}
+		response(toggle());
+	});
+
+	// Let the background script know that we've loaded new content.
+	chrome.runtime.sendMessage('ready', toggle);
+
+	// HACK(riley): To gain an advantage in the specificity wars (against RES,
+	//              for example), add an ID to the <html> or <body> element if
+	//              one doesn't already exist. It's not a perfect solution:
+	//              - If <html> and <body> both have ids, no id is applied.
+	//              - Triples the size of injected CSS, since each line needs a
+	//                html#specificityHelper and body#specificityHelper variant.
+	//
+	//              I do, however, prefer it over some of the alternatives:
+	//              - Multiple space-delimited ids aren't valid.
+	//              - Lone classes don't provide a strong specificity gain.
+	//              - xml:id no-longer matches CSS selectors _or_ getElementById.
+	//              - Wrapping the children of <body> might lead to a mess of
+	//                layout and script problems.
+	//              - Toggling inline styles is a huge pain right now, and though
+	//                it might be the right idea some day it's far beyond the
+	//                scope of this project.
+	var specificityHelper = 'dark-theme-everywhere-specificity-helper';
+	html.classList.add(specificityHelper);
+	document.addEventListener('DOMContentLoaded', function () {
+		var body = document.body;
+		body.classList.add(specificityHelper);
+		if (!html.id) html.id = specificityHelper;
+		else if (!body.id) body.id = specificityHelper;
+	});
+}(this);
